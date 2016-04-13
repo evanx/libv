7d9412d276d4d0f8c39ed4dce970f0823356502f
Evan Summers <evan.summers@gmail.com>
2016-04-13 22:03:25 +0200

    deinit

--- Loggers.js
+++ Loggers.js
@@ -1,5 +1,14 @@
 
-export function createLogger(filename) {
-   var name = filename.match(/([^\/\\]+)\.[a-z0-9]+/)[1];
-   return bunyan.createLogger({name: name, level: global.loggerLevel});
+export function createLogger(filename, level) {
+   let name = filename;
+   const nameMatch = filename.match(/([^\/\\]+)\.[a-z0-9]+/);
+   if (nameMatch) {
+      name = nameMatch[1];
+   }
+   const logger = bunyan.createLogger({name: name, level: level || global.loggerLevel});
+   logger.debug('createLogger', name, level);
+   return Object.assign(logger, {
+      ndebug: () => {
+      }
+   });
 };
