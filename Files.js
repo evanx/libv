7783e4590319376caf3c2f41dd6b8cd419abf20d
Evan Summers <evan.summers@gmail.com>
2016-05-20 18:05:42 +0200

    Files

--- Files.js
+++ Files.js
@@ -9,6 +9,20 @@ export function mkdirp(directory) {
    });
 }
 
+export function stat(file) {
+   return Promises.promisify(callback => fs.stat(file, callback));
+}
+
+export function existsFile(file) {
+   return stat(file).then(stats => stats.isFile()).catch(err => {
+      return false;
+   });
+}
+
 export function readFile(file) {
    return Promises.promisify(callback => fs.readFile(file, callback));
+}
+
+export function writeFile(file, content) {
+   return Promises.promisify(callback => fs.writeFile(file, content, callback));
 }
