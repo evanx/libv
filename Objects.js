4009664367e564b49ed1e8c3d99d3a084c93ebe5
Evan Summers <evan.summers@gmail.com>
2016-05-27 02:24:48 +0200

    script

--- Objects.js
+++ Objects.js
@@ -9,16 +9,3 @@ export function kv(object, key) {
    assert.equal(typeof key, 'string');
    return {key, value: object[key]};
 }
-
-export function translate(object, other, fn) {
-   Object.keys(object).forEach(key => {
-      const entry = fn(key, object[key], other);
-      if (!entry) {
-      } else if (!Values.isDefined(entry.key)) {
-      } else if (!Values.isDefined(entry.value)) {
-      } else {
-         other[entry.key] = entry.value;
-      }
-   });
-   return other;
-}
